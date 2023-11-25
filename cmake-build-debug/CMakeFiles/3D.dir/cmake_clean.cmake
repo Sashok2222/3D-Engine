0122d091cacda60d92d91df2d7a0659bea0aaf90
Sashok2222 <152014640+Sashok2222@users.noreply.github.com>
2023-11-25 16:34:45 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/3D.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "3D.exe"
+  "3D.exe.manifest"
+  "3D.pdb"
+  "CMakeFiles/3D.dir/Camera.cpp.obj"
+  "CMakeFiles/3D.dir/main.cpp.obj"
+  "lib3D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
